6388e079b9d47485c9d3aba4b3bc688421a3fec6
55036662 <zhippuden@gmail.com>
2020-04-25 00:49:58 +0200

    reduced the userTheme with a ternary operator and fixed the contrastText issue on light colors.

--- src/Components/UserThemeProvider.js
+++ src/Components/UserThemeProvider.js
@@ -3,49 +3,22 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/core';
 
 const UserThemeProvider = ({ children, userColor, userTheme }) => {
-	const systemTheme = () => {
-		if (userTheme) {
-			const theme = createMuiTheme({
-				palette: {
-					common: {
-						black: '#000000',
-						white: '#FFFFFF',
-					},
-					type: 'dark',
-					primary: {
-						main: userColor,
-					},
-				},
-			});
-
-			return theme;
-		}
-		if (!userTheme) {
-			const theme = createMuiTheme({
-				palette: {
-					common: {
-						black: '#000000',
-						white: '#FFFFFF',
-					},
-					type: 'light',
-					primary: {
-						main: userColor,
-						contrastText: '#fafafa',
-					},
-					secondary: {
-						main: '#FF4081',
-						contrastText: '#fafafa',
-					},
-				},
-			});
-
-			return theme;
-		}
-	};
+	const theme = createMuiTheme({
+		palette: {
+			common: {
+				black: '#000000',
+				white: '#FFFFFF',
+			},
+			type: userTheme ? 'dark' : 'light',
+			primary: {
+				main: userColor,
+			},
+		},
+	});
 
 	return (
 		<>
-			<ThemeProvider theme={systemTheme()}>{children}</ThemeProvider>
+			<ThemeProvider theme={theme}>{children}</ThemeProvider>
 		</>
 	);
 };
