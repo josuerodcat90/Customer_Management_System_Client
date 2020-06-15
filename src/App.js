81a787bd2f52f7df697344e7160d3dae7e72d042
Josue Rodriguez <55036662+josuerodcat90@users.noreply.github.com>
2020-06-15 12:17:07 +0200

    Revert "Full calendar"

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import { AuthProvider } from './Context/Auth';
-import AuthRoute from './Utils/AuthRoute';
-import UnAuthRoute from './Utils/UnAuthRoute';
-
-import MenuBar from './Components/MenuBar';
-import Home from './Pages/Home';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Appointments from './Pages/Appointments';
-import Patients from './Pages/Patients';
-import NotFound from './Pages/404';
-
+import logo from './logo.svg';
 import './App.css';
 
-const App = (props) => (
-	<AuthProvider>
-		<Router>
-			<MenuBar>
-				<Switch>
-					<UnAuthRoute exact path='/' component={Home} />
-					<AuthRoute exact path='/login' component={Login} />
-					<AuthRoute exact path='/register' component={Register} />
-					<UnAuthRoute exact path='/calendar' component={Appointments} />
-					<UnAuthRoute exact path='/patients' component={Patients} />
-					<Route component={NotFound} />
-				</Switch>
-			</MenuBar>
-		</Router>
-	</AuthProvider>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
